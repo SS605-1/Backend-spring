7076ba7ac4b0afca235e7a3b58ac34e78e85d209
sinam7 <muwon777@naver.com>
2024-08-18 02:08:34 +0900

    feat: 전역 및 컨트롤러 로깅 AOP 추가

---
+++ src/main/java/com/ss6051/backendspring/global/logger/LogAspect.java
@@ -0,0 +1,87 @@
+package com.ss6051.backendspring.global.logger;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@Aspect
+@Slf4j
+@Component
+public class LogAspect {
+
+    // global 패키지 제외 모든 패키지에 대한 로그를 출력한다.
+    @Pointcut("execution(* com.ss6051.backendspring..*.*(..)) && !execution(* com.ss6051.backendspring.global..*.*(..))")
+    public void all() {
+    }
+
+    // 모든 패키지 내 서브패키지 controller 패키지에 대한 로그를 출력한다.
+    @Pointcut("within(com.ss6051.backendspring..*Controller)")
+    public void controller() {
+    }
+
+    @Around("all()")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long end = System.currentTimeMillis();
+            long timeMs = end - start;
+            log.info("{} | Time : {}ms", joinPoint.getSignature(), timeMs);
+        }
+    }
+
+    @Around("controller()")
+    public Object loggingBefore(ProceedingJoinPoint joinPoint) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+
+        String controllerName = joinPoint.getSignature().getDeclaringType().getName();
+        String methodName = joinPoint.getSignature().getName();
+        Map<String, Object> params = new HashMap<>();
+
+        try {
+            String decodedURI = URLDecoder.decode(request.getRequestURI(), StandardCharsets.UTF_8);
+
+            params.put("controller", controllerName);
+            params.put("method", methodName);
+            params.put("params", getParams(request));
+            params.put("body", request.getAttribute("body"));
+            params.put("log_time", System.currentTimeMillis());
+            params.put("request_uri", decodedURI);
+            params.put("http_method", request.getMethod());
+        } catch (Exception e) {
+            log.error("LoggerAspect error", e);
+        }
+
+        log.info("[{}] {}", params.get("http_method"), params.get("request_uri"));
+        log.info("method: {}.{}", params.get("controller") ,params.get("method"));
+        log.info("params: {}", params.get("params"));
+
+        return joinPoint.proceed();
+}
+
+    private static JSONObject getParams(HttpServletRequest request) {
+        JSONObject jsonObject = new JSONObject();
+        Enumeration<String> params = request.getParameterNames();
+        while (params.hasMoreElements()) {
+            String param = params.nextElement();
+            String replaceParam = param.replaceAll("\\.", "-");
+            jsonObject.put(replaceParam, request.getParameter(param));
+        }
+        return jsonObject;
+    }
+}
