c3b0f8de02cc3ae89db54674d70bc2dedc1e8620
sinam7 <muwon777@naver.com>
2024-08-31 01:08:07 +0900

    fix: CORS 허용 메소드에 PATCH 추가
    실제 근무 시간 수정 시 PATCH를 사용하는데 cors에 설정되어있지 않아 오류 발생, 허용 메소드에 patch를 추가해 해결함

--- src/main/java/com/ss6051/backendspring/global/configuration/SecurityConfig.java
+++ src/main/java/com/ss6051/backendspring/global/configuration/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L); // Optional, sets the max age of the CORS preflight cache
